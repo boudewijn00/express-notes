35a1f776341ca2b9b6279bf91badf868d935a94a
Boudewijn Overvliet <boudewijnovervliet@BoudewiacStudio.fritz.box>
2023-06-25 10:00:22 +0200

    implemented handlebars

--- index.js
+++ index.js
@@ -1,42 +1,88 @@
-const mustacheExpress = require('mustache-express');
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.engine('html', mustacheExpress());
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+const handlebars = require('express-handlebars');
+
+app.engine('handlebars', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 require('dotenv').config();
 
 const axios = require('axios');
 const config = {
     headers: {
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoibm9uX2Fub24ifQ.-1LNTzFFIB2YHBdIVjC45H61mbs318hgPh7-JRpXWu0'
+        'Authorization': process.env.POSTGREST_TOKEN,
     }
 };
 
-const getNotes = async (tags) => {
-    console.log(tags);
-    const response = await axios.get('http://localhost:8000/notes?tags=cs.{'+tags+'}', config);
+const getTags = async () => {
+    const response = await axios.get('http://localhost:8000/tags', config);
 
     return response.data;
 };
 
+const getNotes = async (folderId, tags) => {
+    tagsQuery = tags ? '&tags=cs.{' + tags + '}' : '';
+    const response = await axios.get('http://localhost:8000/notes?parent_id=eq.'+folderId+tagsQuery, config);
+
+    return response.data;
+};
+
+const getFolders = async () => {
+    const response = await axios.get('http://localhost:8000/folders', config);
+    
+    return response.data;
+};
+
+const getFolder = async (id) => {
+    const response = await axios.get('http://localhost:8000/folders?folder_id=eq.'+id, config);
+
+    return response.data[0];
+};
+
 app.get('/', (req, res) => {
-    getNotes().then((notes) => {
-        res.send(notes);
-    });
-});
-
-app.get('/mustache', (req, res) => {
-    // get tags from query string
     const tags = req.query.tags;
-    getNotes(tags).then((notes) => {
-        res.render('index.html', { "notes": notes, "tags": tags });
+    getFolders().then((folders) => {
+        res.render('home', {
+            layout : 'main', 
+            folders: folders
+        });
     });
 });
 
-const port = 5000;
+app.get('/folders/:id', (req, res) => {
+    const id = req.params.id;
+    const queryTags = req.query.tags;
+    getTags().then((tags) => {
+        console.log(tags);
+        getFolder(id).then((folder) => {
+            getFolders().then((folders) => {
+                getNotes(id, queryTags).then((notes) => {
+                    res.render('notes', {
+                        layout : 'main', 
+                        folders: folders,
+                        folder: folder,
+                        notes: notes,
+                        tags: tags,
+                        url: req.protocol + '://' + req.get('host') + req.originalUrl,
+                        helpers: {
+                            isBookmarksFolder: function(folder) {
+                                console.log(folder);
+                                return folder.title === 'bookmarks';
+                            }
+                        }
+                    });
+                });
+            });
+        });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
